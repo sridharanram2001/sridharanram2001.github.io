39c321eb71d8f9e5b78d25ecdb94063c76729d71
Ramprakash <sridharanram2001@gmail.com>
2023-03-24 19:19:27 -0700

    Initial commit

---
+++ command_java.js
@@ -0,0 +1,15 @@
+
+function GotoResume()
+{
+
+window.open('https://drive.google.com/file/d/1y-1t2OBXkwWm2Qhrg-4JWFIIyZxvyduq/view?usp=share_link', '_blank').focus();
+
+}
+
+function Projects()
+{
+
+  window.open('Project_Tab.html', '_blank').focus();
+
+}
+
